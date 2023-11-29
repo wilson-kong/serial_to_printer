a5c2bf21a76b3f59afa91c779776d452d40a64e5
Wilson Kong <wilson.kong@uqconnect.edu.au>
2023-11-29 10:17:45 +1000

    script to run 3D printer through serial

---
+++ connect_to_3d_printer.py
@@ -0,0 +1,37 @@
+import serial.tools.list_ports
+import time
+
+PORT = 'COM5'                   # COM port the printer is connected to
+BAUD_RATE = 115200              # Baud rate, should be fixed
+FILE_NAME = 'movement.gcode'    # input file for movement.
+
+# connection to printer
+serial_port = serial.Serial(PORT, BAUD_RATE)
+print(f'{serial_port} is open.')
+
+time.sleep(2)
+
+def parse_gcode(gcode_raw: str):
+    """
+    Pasrses gcode to something readable by the machine.
+    Parameters:
+        gcode_raw (str): line of gcode to be parsed
+    Returns:
+        (str): parsed gcode with a new line charater added.
+    """
+    actual, _, _ = gcode_raw.partition(';')
+    actual = actual.strip()
+    actual += '\n'
+    return actual
+
+# read and process gcode file
+with open(FILE_NAME, 'r') as file:
+    gcode = [parse_gcode(i).encode('ascii') for i in file]
+
+# write to 3D printer
+for code in gcode:
+    serial_port.write(code)
+
+# shut down
+time.sleep(2)
+serial_port.close()
---
+++ movement.gcode
@@ -0,0 +1,10 @@
+G90
+
+G28 ; Home all axis
+G1 Z5 ; Lift Z axis
+G1 X32 Y36 F3000; Move to Position 1
+
+M0 ; Pause print
+
+G28;
+M84 ; disable motors
